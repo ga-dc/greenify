3e3287a7681a65dd0dcbb78678f14f3fae4d4962
Andrew Smith-Mui <arsm800@gmail.com>
2016-03-01 17:20:54 -0500

    change div backgrounds to lime green

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 $(document).ready(function(){
   $("h1").on("click", function(){
-    $("body").append($("<div>jQuery plugins are cool!</div>"))
-  })
+    $("body").append($("<div>jQuery plugins are cool!</div>"));
+  });
   $("h2").on("click", function(){
-    $("div").greenify()
-  })
+    $("div").greenify();
+  });
   $("h3").on("click", function(){
-    $("body").addGreenDiv()
-  })
-  $("h4").on("click", function(){
-    $(".greenified").switchColors()
-  })
+    $("body").addGreenDiv();
+  });
+  $("h4").on("click", function() {
+  $("div").limeify();
+});
 
-
-})
+});
